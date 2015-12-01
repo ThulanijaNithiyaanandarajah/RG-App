441b30dea643c2f8ecda8fbd61033c5889ff7443
GowthamanJeyakumaran <gawthaman.j@findmyfare.lk>
2015-12-01 17:24:01 +0530

    Changed

--- RGApp/app/src/main/java/com/example/user/rgapp/home_page.java
+++ RGApp/app/src/main/java/com/example/user/rgapp/home_page.java
@@ -1,6 +1,6 @@
 package com.example.user.rgapp;
 
-import android.support.v7.app.AppCompatActivity;
+/*import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class home_page extends AppCompatActivity {
@@ -9,5 +9,74 @@ public class home_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+} */
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class home_page extends FragmentActivity {
+    static final int NUM_ITEMS = 6;
+    ImageFragmentPagerAdapter imageFragmentPagerAdapter;
+    ViewPager viewPager;
+    public static final String[] IMAGE_NAME = {"com", "lederboard_kellyfwlder", "lederboard", "ntb", "leaderboard_laptop_carnival", "sampath",};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        imageFragmentPagerAdapter = new ImageFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager) findViewById(R.id.slide);
+        viewPager.setAdapter(imageFragmentPagerAdapter);
+    }
+
+    public static class ImageFragmentPagerAdapter extends FragmentPagerAdapter {
+        public ImageFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            SwipeFragment fragment = new SwipeFragment();
+            return SwipeFragment.newInstance(position);
+        }
+    }
+
+    public static class SwipeFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View swipeView = inflater.inflate(R.layout.activity_view_slider, container, false);
+            ImageView imageView = (ImageView) swipeView.findViewById(R.id.imageView2);
+            Bundle bundle = getArguments();
+            int position = bundle.getInt("position");
+            String imageFileName = IMAGE_NAME[position];
+            int imgResId = getResources().getIdentifier(imageFileName, "drawable", "com.example.user.rgapp");
+            imageView.setImageResource(imgResId);
+            return swipeView;
+        }
+
+        static SwipeFragment newInstance(int position) {
+            SwipeFragment swipeFragment = new SwipeFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("position", position);
+            swipeFragment.setArguments(bundle);
+            return swipeFragment;
+        }
     }
 }
