2bee82d28ef822a3cf2511e432f93936ce9f74f8
GowthamanJeyakumaran <gawthaman.j@findmyfare.lk>
2015-12-02 09:01:36 +0530

    change

--- RGApp/app/src/main/java/com/example/user/rgapp/MainActivity.java
+++ RGApp/app/src/main/java/com/example/user/rgapp/MainActivity.java
@@ -1,19 +1,5 @@
 package com.example.user.rgapp;
 
-/*import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class home_page extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-    }
-} */
-
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -25,18 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class home_page extends FragmentActivity {
+public class MainActivity extends FragmentActivity {
     static final int NUM_ITEMS = 6;
     ImageFragmentPagerAdapter imageFragmentPagerAdapter;
     ViewPager viewPager;
-    public static final String[] IMAGE_NAME = {"com", "lederboard_kellyfwlder", "lederboard", "ntb", "leaderboard_laptop_carnival", "sampath",};
+    public static final String[] IMAGE_NAME = {"com", "leaderboard_kellyfelder", "leaderboard_laptop_carnival", "leaderboard_tv", "ntb", "sampath",};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.fragment_pager);
         imageFragmentPagerAdapter = new ImageFragmentPagerAdapter(getSupportFragmentManager());
-        viewPager = (ViewPager) findViewById(R.id.slide);
+        viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(imageFragmentPagerAdapter);
     }
 
@@ -61,12 +47,12 @@ public class home_page extends FragmentActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View swipeView = inflater.inflate(R.layout.activity_view_slider, container, false);
-            ImageView imageView = (ImageView) swipeView.findViewById(R.id.imageView2);
+            View swipeView = inflater.inflate(R.layout.swipe_fragment, container, false);
+            ImageView imageView = (ImageView) swipeView.findViewById(R.id.imageView);
             Bundle bundle = getArguments();
             int position = bundle.getInt("position");
             String imageFileName = IMAGE_NAME[position];
-            int imgResId = getResources().getIdentifier(imageFileName, "drawable", "com.example.user.rgapp");
+            int imgResId = getResources().getIdentifier(imageFileName, "drawable", "com.javapapers.android.swipeimageslider");
             imageView.setImageResource(imgResId);
             return swipeView;
         }
